02c32aa0f58d255f8af67685e3cbe49c62533a4b
jayraor <54411229+jayraor@users.noreply.github.com>
2019-09-17 18:21:44 +0200

    Create expression-4.cpp

---
+++ week1/expression-4.cpp
@@ -0,0 +1,21 @@
+/**
+ * expressions-4.cpp: Modulo 
+ */
+
+#include <iostream>
+
+int main(void)
+{
+    // User soll Eingabe machen
+    std::cout<<"Enter a number to take modulo: ";
+    int num;
+    std::cin>>num;
+    
+    // Eine weitere Eingabe 
+    std::cout<<"Enter number that one should take modulo with: ";
+    int mod;
+    std::cin>>mod;
+    
+    // Ausgabe von Modulo
+    std::cout<<"Das Ergebnis ist: "<<mod<<std::endl;
+}
