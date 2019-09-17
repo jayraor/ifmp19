69f9ca2a28d864d2e15dbd56480fb6d1da516616
jayraor <54411229+jayraor@users.noreply.github.com>
2019-09-17 16:22:21 +0200

    Updated calc.cpp
    more or less "add.cpp"

--- week1/calc.cpp
+++ week1/calc.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+
+int main(void)
+{
+   std::cout<<"Please give me a number: ";
+   int num = 0;
+   std::cin>>num;
+   std::cout<<"Please give me another number";
+   int num2 = 0;
+   std::cin>>num2;
+   std::cout<<"The sum is: " << num + num2 << std::endl;
+  
+   return 0;
+}
