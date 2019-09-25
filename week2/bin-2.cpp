d98e90d068cad8421a6a9da315ec26995b20083a
jayraor <54411229+jayraor@users.noreply.github.com>
2019-09-25 11:17:11 +0200

    Create bin-2.cpp

---
+++ week2/bin-2.cpp
@@ -0,0 +1,36 @@
+/**
+ * bin-2.cpp: Uses the tutorial and improves design
+ */
+ 
+ 
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(void)
+{
+    // declare variable
+    int a;
+    cin>>a;
+     
+    int size = 1;
+    int counter = 0;
+    // use loop to get size
+    while(a / size != 0)
+    {
+        size *= 2;
+        counter++;
+    }
+ 
+    while (counter >= 0)
+    {   
+        cout<<a/size;
+        counter--;
+        if (a / size == 1)
+        {
+            a = a - size;
+        }
+        size /= 2;
+    }
+ 
+}
